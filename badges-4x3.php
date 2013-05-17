d2b56199ae8495c214ee229b69396c9ef1629afc
ninnypants <tyrel@ninnypants.com>
2013-05-17 01:00:54 -0600

    Dump progress so far. Everything basically works just need to add styles for badges and maybe some qr code generation.

---
+++ badges-4x3.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8">
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html>
+<!--<![endif]-->
+<head>
+	<title></title>
+<style type="text/css">
+	/* http://meyerweb.com/eric/tools/css/reset/
+	   v2.0 | 20110126
+	   License: none (public domain)
+	*/
+
+	html, body, div, span, applet, object, iframe,
+	h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+	a, abbr, acronym, address, big, cite, code,
+	del, dfn, em, img, ins, kbd, q, s, samp,
+	small, strike, strong, sub, sup, tt, var,
+	b, u, i, center,
+	dl, dt, dd, ol, ul, li,
+	fieldset, form, label, legend,
+	table, caption, tbody, tfoot, thead, tr, th, td,
+	article, aside, canvas, details, embed,
+	figure, figcaption, footer, header, hgroup,
+	menu, nav, output, ruby, section, summary,
+	time, mark, audio, video {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		font-size: 100%;
+		font: inherit;
+		vertical-align: baseline;
+	}
+
+	/* HTML5 display-role reset for older browsers */
+	article, aside, details, figcaption, figure,
+	footer, header, hgroup, menu, nav, section {
+		display: block;
+	}
+
+	body {
+		line-height: 1;
+	}
+
+	ol, ul {
+		list-style: none;
+	}
+
+	blockquote, q {
+		quotes: none;
+	}
+
+	blockquote:before, blockquote:after,
+	q:before, q:after {
+		content: '';
+		content: none;
+	}
+
+	table {
+		border-collapse: collapse;
+		border-spacing: 0;
+		width: 21.59cm;
+		height: 27.94cm;
+	}
+
+	td {
+		overflow: hidden;
+	}
+
+	.side-rule {
+		width: 0.6349996571001851cm;
+	}
+
+	.top-rule {
+		width: 10.159994513602962cm;
+		height: 2.5399986284007405cm;
+	}
+
+	.badge {
+		width: 10.159994513602962cm;
+		height: 7.619995885202221cm;
+	}
+</style>
+</head>
+<body>
+<table>
+	<tr><td class="side-rule"></td><td class="top-rule"></td><td class="top-rule"></td><td class="side-rule"></td></tr>
+	<?php
+	global $badges;
+	echo $badges;
+	?>
+	<!-- <tr>
+		<td class="side-rule"></td>
+		<td class="badge">
+			<h1>{$first_name[0]} {$last_name[0]}</h1>
+			<h2>{$job_title[0]}</h2>
+			<h2>{$company[0]}</h2>
+			<img src="http://www.openwest.org/wp-content/uploads/2013/02/open-west-conference-logo.png" width="100px;">
+		</td>
+		<td class="badge">
+			<h1>{$first_name[1]} {$last_name[1]}</h1>
+			<h2>{$job_title[1]}</h2>
+			<h2>{$company[1]}</h2>
+			<img src="http://www.openwest.org/wp-content/uploads/2013/02/open-west-conference-logo.png" width="100px;">
+		</td>
+		<td class="side-rule"></td>
+	</tr>
+	<tr>
+		<td class="side-rule"></td>
+		<td class="badge">
+			<h1>{$first_name[2]} {$last_name[2]}</h1>
+			<h2>{$job_title[2]}</h2>
+			<h2>{$company[2]}</h2>
+			<img src="http://www.openwest.org/wp-content/uploads/2013/02/open-west-conference-logo.png" width="100px;">
+		</td>
+		<td class="badge">
+			<h1>{$first_name[3]} {$last_name[3]}</h1>
+			<h2>{$job_title[3]}</h2>
+			<h2>{$company[3]}</h2>
+			<img src="http://www.openwest.org/wp-content/uploads/2013/02/open-west-conference-logo.png" width="100px;">
+		</td>
+		<td class="side-rule"></td>
+	</tr>
+	<tr>
+		<td class="side-rule"></td>
+		<td class="badge">
+			<h1>{$first_name[4]} {$last_name[4]}</h1>
+			<h2>{$job_title[4]}</h2>
+			<h2>{$company[4]}</h2>
+			<img src="http://www.openwest.org/wp-content/uploads/2013/02/open-west-conference-logo.png" width="100px;">
+		</td>
+		<td class="badge">
+			<h1>{$first_name[5]} {$last_name[5]}</h1>
+			<h2>{$job_title[5]}</h2>
+			<h2>{$company[5]}</h2>
+			<img src="http://www.openwest.org/wp-content/uploads/2013/02/open-west-conference-logo.png" width="100px;">
+		</td>
+		<td class="side-rule"></td>
+	</tr> -->
+	<tr><td class="side-rule"></td><td class="top-rule"></td><td class="top-rule"></td><td class="side-rule"></td></tr>
+</table>
+</body>
+</html>
--- badges.php
+++ badges.php
@@ -1,8 +1,11 @@
 <?php
+error_reporting( E_ALL );
 require 'eventbrite/Eventbrite.php';
-require 'wkhtmltopdf/WkHtmlToPdf.php';
+require 'phpwkhtmltopdf/WkHtmlToPdf.php';
 
-$wkhtmltopdf_bin = 'wkhtmltopdf-i386';
+$wkhtmltopdf_bin = dirname( __FILE__ ) . '/wkhtmltopdf-i386';
+
+$template_file = 'badges-4x3.php';
 
 $keys = array(
 	'app_key' => '',
@@ -16,4 +19,60 @@ $attendee_list_args = array(
 
 $eb = new Eventbrite( $keys );
 
-$attendees = $eb->event_listAttendees( $attendee_list_args );
+try {
+	$attendees = $eb->event_list_attendees( $attendee_list_args );
+	$attendees = $attendees->attendees;
+} catch (Exception $e) {
+	var_dump( $e );
+}
+
+$pdf = new WkHtmlToPdf( array(
+	'bin' => $wkhtmltopdf_bin,
+	'page-size' => 'Letter',
+	'margin-top' => 0,
+	'margin-right' => 0,
+	'margin-bottom' => 0,
+	'margin-left' => 0,
+) );
+
+// $template = file_get_contents( $template_file );
+$badges = '';
+$num = 1;
+foreach( $attendees as $attendee ){
+	// have to drop it in one more because event brite
+	// thinks it makes sense to put an object as the
+	// only value in an object
+	$attendee = $attendee->attendee;
+	// $template = str_replace( array(
+	// 	'{$first_name[' . $num . ']}',
+	// 	'{$last_name[' . $num . ']}',
+	// 	'{$job_title[' . $num . ']}',
+	// 	'{$company[' . $num . ']}',
+	// ), array(
+	// 	$attendee->first_name,
+	// 	$attendee->last_name,
+	// 	$attendee->job_title,
+	// 	$attendee->company,
+	// ), $template );
+
+	if( $num % 2 || 1 === $num )
+		$badges .= '<tr><td class="side-rule"></td>';
+	$badges .= '<td>';
+	$badges .= '<h1>' . $attendee->first_name . ' ' . $attendee->last_name . '</h1>' .
+	'<h2>' . $attendee->job_title . '</h2>' .
+	'<h2>' . $attendee->company . '</h2>' .
+	'<img src="http://www.openwest.org/wp-content/uploads/2013/02/open-west-conference-logo.png" width="100px;">';
+	$badges .= '<td>';
+	if( ! $num % 2 )
+		$badges .= '<td class="side-rule"></td></tr>';
+	$num++;
+}
+ob_start();
+include $template_file;
+$template = ob_get_clean();
+
+// $pdf->addPage( $template );
+// if( !$pdf->send() )
+// 	var_dump( $pdf->getError() );
+
+echo $template;
